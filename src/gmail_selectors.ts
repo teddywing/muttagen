b699d46f1e10fb65b57a525c9f54701ceafa1478
Teddy Wing <git@teddywing.com>
2019-05-01 19:35:28 +0200

    gmail_selectors.ts: Add new selectors for index and pager views

--- src/gmail_selectors.ts
+++ src/gmail_selectors.ts
@@ -1,5 +1,17 @@
 export type GmailCSSClass = string;
+type GmailSelector = string;
 
 export const TOOLS_PANEL: GmailCSSClass = 'bAw';
 export const SIDEBAR: GmailCSSClass = 'aeN';
 export const MESSAGE_PAGER: GmailCSSClass = 'AO';
+
+export const INDEX: GmailSelector = '.BltHke.nH.oy8Mbf[role="main"]';
+export const PAGER: GmailSelector = '.nH.g.id';
+
+
+// Index:
+// .BltHke.nH.oy8Mbf[role="main"]
+//
+// Pager:
+// .nH[role="main"]
+// .nH.g.id
