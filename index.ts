6f12d5feee09725cea71a741ddac4acad0ffbb24
Teddy Wing <git@teddywing.com>
2019-05-01 14:30:17 +0200

    Change `MESSAGE_PAGER` class
    Use a class from a deeper element in the hierarchy. This way the action
    buttons above messages aren't inverted.

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ type GmailCSSClass = string;
 
 const TOOLS_PANEL: GmailCSSClass = 'bAw';
 const SIDEBAR: GmailCSSClass = 'aeN';
-const MESSAGE_PAGER: GmailCSSClass = 'nH';
+const MESSAGE_PAGER: GmailCSSClass = 'AO';
 
 
 type GmailCSSDefinitions = { [selector in GmailCSSClass]: string };
